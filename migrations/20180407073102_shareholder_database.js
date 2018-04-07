106cdf333082ef8c4b3f07c2ed661b5691ab188c
mark-sprott <sprotty01@gmail.com>
2018-04-07 13:18:12 +1200

    migrations and seeds for shareholder details started

--- migrations/20180407073102_shareholder_database.js
+++ migrations/20180407073102_shareholder_database.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+    return knex.schema.createTableIfNotExists('shareholder_details', (table) => {
+      table.increments('id').primary()
+      table.string('first_name')
+      table.string('last_name')
+      table.increments('shareholder_number')
 
-exports.down = function(knex, Promise) {
+    })
+  }
   
-};
+  exports.down = (knex, Promise) => {
+    return knex.schema.dropTableIfExists('shareholder_details')
+  }
--- seeds/shareholder_details.js
+++ seeds/shareholder_details.js
@@ -1,13 +1,17 @@
 
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('shareholder_names').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('shareholder_names').insert([
+        {id: 1, shareholder_number: '123456', Shareholder_name: 'Mark Sprott'},
+        {id: 2, shareholder_number: '789012', Shareholder_name: 'Tom Scott'},
+        {id: 3, shareholder_number: '890123', Shareholder_name: 'John Doe'}
       ]);
     });
 };
+
+
+
